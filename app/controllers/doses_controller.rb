ba291295e88747d73096e6b34bc2a6f73878bd59
Quentin Roquigny <roquignyq@gmail.com>
2016-05-12 17:24:40 +0200

    end of today

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,28 +1,29 @@
 class DosesController < ApplicationController
   def index
-
   end
 
   def show
-
   end
 
   def new
-
   end
 
   def create
     @dose = Dose.new(dose_params)
-    ingredient = Ingredient.find(params[:dose][:ingredient].to_i)
-    @dose.ingredient = ingredient
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @cocktail
 
-    # @review = @restaurant.review.build(review_params)
+    if params[:dose][:ingredient].to_i > 0
+      ingredient = Ingredient.find(params[:dose][:ingredient].to_i)
+      @dose.ingredient = ingredient
 
-    @dose.save
+      @dose.cocktail = @cocktail
+      @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      # redirect_to cocktail_path(@cocktail)
+      render 'cocktails/show'
+    end
 
-    redirect_to cocktail_path(@cocktail)
     # if @dose.save
     #   redirect_to cocktail_path(@cocktail)
     #   else
